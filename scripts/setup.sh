6293f5b805d3f13282491138a42ffb9f00fba0a0
proelbtn <contact@proelbtn.com>
2020-03-08 21:53:48 +0000

    add procedure to install kernel module

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,18 @@
 
 sudo echo -n
 
+sudo apt install -y \
+	linux-buildinfo-5.3.0-23-generic \
+	linux-headers-5.3.0-23-generic \
+	linux-image-5.3.0-23-generic \
+	linux-modules-5.3.0-23-generic \
+	linux-modules-extra-5.3.0-23-generic \
+	linux-tools-5.3.0-23-generic
+
 if ! which docker >/dev/null 2>&1; then
   curl -L get.docker.com | sudo sh
   sudo usermod -aG docker $(id -un)
 fi
+
+sudo mount bpffs /sys/fs/bpf -t bpf
 
